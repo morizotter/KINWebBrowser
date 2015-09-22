a0385e9d055f3daf21a24df18e3fc1e469d5e989
Naoki Morita <namorit@gmail.com>
2015-09-22 16:47:10 +0900

    Add KINWebBrowser.xcodeproj

---
+++ KINWebBrowser/KINWebBrowser.h
@@ -0,0 +1,19 @@
+//
+//  KINWebBrowser.h
+//  KINWebBrowser
+//
+//  Created by MORITANAOKI on 2015/09/22.
+//  Copyright (c) 2015年 molabo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KINWebBrowser.
+FOUNDATION_EXPORT double KINWebBrowserVersionNumber;
+
+//! Project version string for KINWebBrowser.
+FOUNDATION_EXPORT const unsigned char KINWebBrowserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KINWebBrowser/PublicHeader.h>
+
+#import <KINWebBrowser/KINWebBrowserViewController.h>
